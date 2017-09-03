57317b5804c7ed01e4b021523c9219b5460549e7
Shyam <pindoria.shyam@gmail.com>
2017-09-03 15:24:08 +0930

    Initial Commit

---
+++ Auto Navigator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Auto Navigator
+//
+//  Created by Shyam Pindoria on 3/9/17.
+//  Copyright © 2017 Shyam Pindoria. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
