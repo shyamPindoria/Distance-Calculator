c7fd2e7a172c6da0f0049da66a69464190be3a17
Shyam <pindoria.shyam@gmail.com>
2017-09-19 17:13:09 +0930

    Populated table view controller

--- Distance Calculator/SettingsViewController.swift
+++ Distance Calculator/SettingsViewController.swift
@@ -10,9 +10,51 @@ import UIKit
 
 class SettingsTableViewController: UITableViewController {
     
+    @IBOutlet var measuringSystem: UISegmentedControl!
+    @IBOutlet var diameter: UISegmentedControl!
+    @IBOutlet var distance: UISegmentedControl!
+    
+    var settings: [String: Int] = ["system": 0, "diameter": 1, "distance": 1]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        updateUnits()
+    }
+    
+    @IBAction func settingsChanged(_ sender: UISegmentedControl) {
+        
+        self.settings.updateValue(measuringSystem.selectedSegmentIndex, forKey: "system")
+        
+        self.settings.updateValue(diameter.selectedSegmentIndex, forKey: "diameter")
+        
+        self.settings.updateValue(distance.selectedSegmentIndex, forKey: "distance")
+        
+       self.updateUnits()
         
     }
     
+    func updateUnits() {
+        
+        if(settings["system"] == 1) {
+            
+            diameter.setTitle("Inch", forSegmentAt: 0)
+            diameter.setTitle("Feet", forSegmentAt: 1)
+            
+            distance.setTitle("Inch", forSegmentAt: 0)
+            distance.setTitle("Feet", forSegmentAt: 1)
+            
+            
+        } else {
+            diameter.setTitle("Millimetres", forSegmentAt: 0)
+            diameter.setTitle("Metres", forSegmentAt: 1)
+            
+            distance.setTitle("Millimetres", forSegmentAt: 0)
+            distance.setTitle("Metres", forSegmentAt: 1)
+        }
+        
+        measuringSystem.selectedSegmentIndex = settings["system"]!
+        diameter.selectedSegmentIndex = settings["diameter"]!
+        distance.selectedSegmentIndex = settings["distance"]!
+    
+    }
 }
--- Distance Calculator/ViewController.swift
+++ Distance Calculator/ViewController.swift
@@ -83,8 +83,7 @@ class ViewController: UIViewController {
     
     func initialize() {
         
-        //Creates the gradient for the view
-        
+        //Create the gradient for the topView
         //1st color
         let colorTop = UIColor(red: 230.0 / 255.0, green: 62.0 / 255.0, blue: 59.0 / 255.0, alpha: 1.0).cgColor
         //2nd color
@@ -97,11 +96,12 @@ class ViewController: UIViewController {
         gradient.endPoint = CGPoint(x: 0.0, y: 1.0)
         //Assign the gradient to the view
         self.topView.layer.insertSublayer(gradient, at: 0)
+        self.topView.layer.cornerRadius = 100
         
         self.mainView.layer.shadowOpacity = 1
         self.mainView.layer.shadowRadius = 10
         
-        self.topView.layer.cornerRadius = 100
+        
         
     }
     
